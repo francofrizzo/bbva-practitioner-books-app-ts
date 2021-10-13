b52d9168c8716900df8f0732fd97c8287b8b8a18
Franco Frizzo <francofrizzo@gmail.com>
2021-10-13 13:44:34 -0300

    Change preloaded books

---
+++ src/utils/books.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    title: 'El Aleph',
+    publication: 1949,
+    author: 'Jorge Luis Borges',
+    summary:
+      'Es uno de los libros de cuentos más representativos del escritor argentino Jorge Luis Borges. Publicado en 1949, fue reeditado por el autor en 1974. Sus textos remiten a una infinidad de fuentes y bibliografías en torno a las cuales se articulan mitos y metáforas de la tradición literaria universal. Esta obra marca un punto de inflexión respecto al estilo que destilaba su colección anterior de cuentos, Ficciones; aun manteniendo su estilo sobrio y perfeccionista, el escritor aborda aquí otra serie de eventos u objetos inverosímiles enmarcados en un ambiente realista, lo que contribuye a resaltar su carácter fantástico.',
+  },
+  {
+    title: 'Rayuela',
+    publication: 1963,
+    author: 'Rayuela',
+    summary:
+      'Narra la historia de Horacio Oliveira, su protagonista, y su relación con «la Maga». La historia pone en juego la subjetividad del lector y tiene múltiples finales. A esta obra suele llamársela «antinovela», aunque el mismo Cortázar prefería denominarla «contranovela». Significó un salto al vacío que lo distanció de la seguridad controlada de los cuentos fantásticos de su primera época como escritor para adentrarse en una búsqueda sin hallazgos a través de preguntas sin respuesta.',
+  },
+];
